19bcb384443e7b2b39da9187db26f994a5b949b3
syshin0228 <syshin0228@utexas.edu>
2020-07-20 10:19:24 -0500

    Machine Learning Algorithms

---
+++ Machine_Learning_6MWT/ML_Analysis_Linear_Regression/main_Linear_Regression_Analysis_FIM.m
@@ -0,0 +1,63 @@
+clc
+clear all
+close all
+
+
+AoM = import_data('./FIM_AoM_only.csv')
+Gyr = import_data('./FIM_Gyro_only.csv')
+Acc = import_data('./FIM_Acc_only.csv')
+AoM_Gyr = import_data('./FIM_AoM_Gyro.csv')
+AoM_Acc = import_data('./FIM_AoM_Acc.csv')
+Gyr_Acc = import_data('./FIM_Gyro_Acc.csv')
+AoM_Gyr_Acc = import_data('./FIM_AoM_Gyro_Acc.csv')
+
+
+lw = 1.2
+mk = 8
+x = [10 20 30 60 90 120 180 240 300 360]
+figure(1)
+plot(x,AoM.MAE_COT_mean,'r','linewidth',lw)
+hold on
+plot(x,Gyr.MAE_COT_mean,'m','linewidth',lw)
+plot(x,Acc.MAE_COT_mean,'b','linewidth',lw)
+% plot(x,AoM_Gyr.MAE_COT_mean,'k','linewidth',lw)
+% plot(x,AoM_Acc.MAE_COT_mean,'k','linewidth',lw)
+% plot(x,Gyr_Acc.MAE_COT_mean,'k','linewidth',lw)
+plot(x,AoM_Gyr_Acc.MAE_COT_mean,'k','linewidth',lw)
+h1 = plot(x,AoM.MAE_COT_mean,'rx','linewidth',lw,'markersize',mk)
+h2 = plot(x,Gyr.MAE_COT_mean,'mo','linewidth',lw,'markersize',mk)
+h3 = plot(x,Acc.MAE_COT_mean,'bd','linewidth',lw,'markersize',mk)
+% plot(x,AoM_Gyr.MAE_COT_mean,'k','linewidth',lw,'markersize',mk)
+% plot(x,AoM_Acc.MAE_COT_mean,'k','linewidth',lw,'markersize',mk)
+% plot(x,Gyr_Acc.MAE_COT_mean,'k','linewidth',lw,'markersize',mk)
+h4 = plot(x,AoM_Gyr_Acc.MAE_COT_mean,'k^','linewidth',lw,'markersize',mk)
+legend([h1, h2, h3, h4],'AoM','Gyr','Acc','All')
+xlabel('Cut off time [sec]')
+ylabel('FIM MAE')
+set(gca,'fontsize',15)
+
+
+figure(2)
+plot(x,AoM.MAEP_COT_mean,'r','linewidth',lw)
+hold on
+plot(x,Gyr.MAEP_COT_mean,'m','linewidth',lw)
+plot(x,Acc.MAEP_COT_mean,'b','linewidth',lw)
+% plot(x,AoM_Gyr.MAEP_COT_mean,'k','linewidth',lw)
+% plot(x,AoM_Acc.MAEP_COT_mean,'k','linewidth',lw)
+% plot(x,Gyr_Acc.MAEP_COT_mean,'k','linewidth',lw)
+plot(x,AoM_Gyr_Acc.MAEP_COT_mean,'k','linewidth',lw)
+h1 = plot(x,AoM.MAEP_COT_mean,'rx','linewidth',lw,'markersize',mk)
+h2 = plot(x,Gyr.MAEP_COT_mean,'mo','linewidth',lw,'markersize',mk)
+h3 = plot(x,Acc.MAEP_COT_mean,'bd','linewidth',lw,'markersize',mk)
+% plot(x,AoM_Gyr.MAEP_COT_mean,'k','linewidth',lw,'markersize',mk)
+% plot(x,AoM_Acc.MAEP_COT_mean,'k','linewidth',lw,'markersize',mk)
+% plot(x,Gyr_Acc.MAEP_COT_mean,'k','linewidth',lw,'markersize',mk)
+h4 = plot(x,AoM_Gyr_Acc.MAEP_COT_mean,'k^','linewidth',lw,'markersize',mk)
+legend([h1, h2, h3, h4],'AoM','Gyr','Acc','All')
+xlabel('Cut off time [sec]')
+ylabel('FIM MAE% [%]')
+set(gca,'fontsize',15)
+
+
+
+
